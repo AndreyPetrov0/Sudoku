1f40b5e7d1b1c69b612d03edb07c50b62136e14c
Lenfer <113467858+AndreyPetrov0@users.noreply.github.com>
2024-06-21 17:05:56 +0300

    Update Validate_Sudoku.py

--- Validate_Sudoku.py
+++ Validate_Sudoku.py
@@ -59,3 +59,66 @@ class Validate_sudoku(object):
             if len(diff_num) != len(enter_sudoku[0]):
                 return False
         return True
+
+if __name__ == '__main__':
+    # Valid Sudoku
+    goodSudoku1 = Validate_sudoku([
+        [7, 8, 4, 1, 5, 9, 3, 2, 6],
+        [5, 3, 9, 6, 7, 2, 8, 4, 1],
+        [6, 1, 2, 4, 3, 8, 7, 5, 9],
+
+        [9, 2, 8, 7, 1, 5, 4, 6, 3],
+        [3, 5, 7, 8, 4, 6, 1, 9, 2],
+        [4, 6, 1, 9, 2, 3, 5, 8, 7],
+
+        [8, 7, 6, 3, 9, 4, 2, 1, 5],
+        [2, 4, 3, 5, 6, 1, 9, 7, 8],
+        [1, 9, 5, 2, 8, 7, 6, 3, 4]
+    ])
+
+    goodSudoku2 = Validate_sudoku([
+        [1, 4, 2, 3],
+        [3, 2, 4, 1],
+
+        [4, 1, 3, 2],
+        [2, 3, 1, 4]
+    ])
+
+    # Invalid Sudoku
+    badSudoku1 = Validate_sudoku([
+        [0, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 1, 9],
+
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 1, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ])
+
+    badSudoku2 = Validate_sudoku([
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+        [1]
+    ])
+
+    badSudoku3 = Validate_sudoku([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                  [2, 3, 1, 5, 6, 4, 8, 9, 7],
+                                  [3, 1, 2, 6, 4, 5, 9, 7, 8],
+                                  [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                                  [5, 6, 4, 8, 9, 7, 2, 3, 1],
+                                  [6, 4, 5, 9, 7, 8, 3, 1, 2],
+                                  [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                                  [8, 9, 7, 2, 3, 1, 5, 6, 4],
+                                  [9, 7, 8, 3, 1, 2, 6, 4, 5]])
+
+
+    print(goodSudoku1.is_valid())    # True, 'Testing valid 9x9'
+    print(goodSudoku2.is_valid())    # True, 'Testing valid 4x4'
+    print(badSudoku1.is_valid())     # False, 'Values in wrong order'
+    print(badSudoku2.is_valid())     # False, '4x5 (invalid dimension)'
+    print(badSudoku3.is_valid())     # False little squares validate
