ce5f66e93d01be5981ff345a3aeb6166c71873b5
Sencaput <113495461+Sencaput@users.noreply.github.com>
2024-08-30 11:43:48 +0300

    Update Functions.py
    Крайняя версия 30.08.24

--- Functions.py
+++ Functions.py
@@ -1,5 +1,6 @@
 from Settings import *
 from jnius import autoclass
+from random import randint
 
 
 def show_android_keyboard():
@@ -29,12 +30,17 @@ def get_y(r):
 	return y
 
 
-def set_new_sudoku(collection, sudoku):
+def set_new_sudoku(collection, sudoku, default):
 	for i in range(len(sudoku)):
 		for j in range(len(sudoku[i])):
 			number = sudoku[i][j]
 			tile = collection.get((i, j))
-			tile.current_color =  tile.locked
+			if default:
+				tile.current_color = bool(number)
+			else:
+				if not tile.current_number:
+					tile.locked = False
+					tile.current_color = False
 			tile.current_number = number
 
 
@@ -44,3 +50,63 @@ def get_cords():
 			continue
 		row, column = cord
 		yield [row, column, object.current_number]
+
+
+def get_square(sudoku_list, square_num):
+	for row in range(9):
+		if row // 3 != square_num // 3:
+			continue
+		for column in range(9):
+			if column // 3 != square_num % 3:
+				continue
+			yield sudoku_list[row][column]
+
+
+def get_field(complete_sudoku: list):
+	field =[]
+	for square in range(9):
+		field.append(list(get_square(complete_sudoku, square)))
+	return field
+
+
+def get_new_sudoku_list(field: list):
+	r0 = []
+	r1 = []
+	r2 = []
+	r3 = []
+	r4 = []
+	r5 = []
+	r6 = []
+	r7 = []
+	r8 = []
+	result = [r0, r1, r2, r3, r4, r5, r6, r7, r8]
+	
+	for result_row in range(9):
+		for square in range(9):
+			for tile in range(9):
+				if result_row == (square // 3) * 3 + tile // 3:
+					result[result_row].append(field[square][tile])
+		
+	return result
+
+
+def get_incomplete_sudoku(sudoku: list):
+	field = get_field(sudoku)
+	new_field = field.copy()
+	for square in range(9):
+		for i in range(difficulty_level):
+			del_tile = randint(0, 8)
+			new_square = new_field[square].copy()
+			new_square[del_tile] = 0
+			new_field[square] = new_square.copy()
+	return get_new_sudoku_list(new_field)
+
+
+def refresh_outer(color):
+	pg.draw.rect(outer_h_surf, color, outer_h_rect)
+	pg.draw.rect(outer_v_surf, color, outer_v_rect)
+
+	screen.blit(outer_h_surf, outer_h1)
+	screen.blit(outer_h_surf, outer_h2)
+	screen.blit(outer_v_surf, outer_v1)
+	screen.blit(outer_v_surf, outer_v2)
